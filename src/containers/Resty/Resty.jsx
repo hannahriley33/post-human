5336e2b92b9bcab53fc2ed32bcb2fcf57502c040
Hannah Riley <riley.hannahm@gmail.com>
2020-05-12 18:15:55 -0700

    history item added

--- src/containers/Resty/Resty.jsx
+++ src/containers/Resty/Resty.jsx
@@ -1,10 +1,15 @@
 import React, { useState } from 'react';
 import Request from '../../components/Request/Request';
+import Response from '../../components/Response/Response';
+import { makeRequest } from '../../services/makeRequest';
+import HistoryList from '../../components/History/HistoryList';
 
 export const Resty = () => {
   const [url, setUrl] = useState('');
   const [method, setMethod] = useState('PUT');
   const [body, setBody] = useState('');
+  const [response, setResponse] = useState({});
+  const [history, setHistory] = useState([]);
 
   const handleChange = ({ target }) => {
     if(target.name === 'url') setUrl(target.value);
@@ -14,10 +19,13 @@ export const Resty = () => {
 
   const handleSubmit = e => {
     e.preventDefault();
+    makeRequest(url, method, body)
+      .then(json => setResponse(json));
   };
 
   return (
     <>
+      <HistoryList history={history} />
       <Request 
         url={url}
         method={method}
@@ -25,6 +33,7 @@ export const Resty = () => {
         onChange={handleChange}
         onSubmit={handleSubmit}
       />
+      <Response response={response} />
     </>
   );
 };
